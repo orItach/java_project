092973f71869c27a78551c72454331dbd9f617eb
orItach <or_ith@walla.com>
2016-11-30 11:03:27 +0200

    secondCommit

--- app/src/main/java/project/java/contact_ptovider/Contact_Provider.java
+++ app/src/main/java/project/java/contact_ptovider/Contact_Provider.java
@@ -3,12 +3,18 @@ package project.java.contact_ptovider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import javax.sql.DataSource;
+
+import project.java.contact_ptovider.backend.IDataSource;
+import project.java.contact_ptovider.backend.ManagerFactory;
+import project.java.contact_ptovider.datasorce.ListDsManager;
+
 public class Contact_Provider extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact__provider);
+        ManagerFactory mf =new ManagerFactory();
     }
-
 }
